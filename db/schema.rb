c0b66aa1308e91198d45e66fb5c58e219a1a6b26
zelzhan <elzhan.zeinulla@nu.edu.kz>
2017-07-01 15:24:13 +0600

    2nd commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170701091203) do
+ActiveRecord::Schema.define(version: 20170701091753) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -78,6 +78,15 @@ ActiveRecord::Schema.define(version: 20170701091203) do
     t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password"
+    t.datetime "created_at",            null: false
+    t.datetime "updated_at",            null: false
+    t.string   "password_confirmation"
   end
 
 end
